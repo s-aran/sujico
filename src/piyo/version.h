e2301cde2b394dd6f53107f54f83d8093f3cd8e2
s.aran <57449457+s-aran@users.noreply.github.com>
2025-03-15 17:09:59 +0900

    hg -> git

--- src/piyo/version.h
+++ src/piyo/version.h
@@ -5,14 +5,12 @@ struct Version
 {
   static inline constexpr auto Major            = "0";
   static inline constexpr auto Minor            = "00";
-  static inline constexpr auto Build            = "269";
+  static inline constexpr auto Build            = "293";
   static inline constexpr auto Milestone        = "beta";
   static inline constexpr auto MilestoneVersion = "3";
-  static inline constexpr auto VcsRevision      = "34";
-  static inline constexpr auto VcsHash          = "72d0d0348def";
-  static inline constexpr auto datetime         = "2019/12/31 23:48:47";
-  static inline constexpr auto tag              = "tip";
-  static inline constexpr auto VersionString    = "0.00.269 beta-3 (34:72d0d0348def)";
+  static inline constexpr auto VcsHash          = "0970b7e";
+  static inline constexpr auto datetime         = "2022/06/08 00:55:14";
+  static inline constexpr auto VersionString    = "0.00.293 beta-3";
 
 
   static inline auto get(const std::string& pre = "", const std::string& post = "", const std::string& versionString = "version: ")
